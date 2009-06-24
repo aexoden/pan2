665e75392208e198ac80410ba3d230718b0d3e01
K. Haley <haleykd@users.sf.net>
2009-06-24 13:09:49 -0600

    Ack more gmime fixes.

--- pan/gui/body-pane.cc
+++ pan/gui/body-pane.cc
@@ -1527,22 +1527,13 @@ namespace
   void get_utf8_body_foreach_part (GMimeObject* /*parent*/, GMimeObject *o
     , gpointer user_data)
   {
-    if (GMIME_IS_MULTIPART(o))
+    GMimePart * part = GMIME_PART (o);
+    GMimeContentType * type = g_mime_object_get_content_type (o);
+    const bool is_text (g_mime_content_type_is_type (type, "text", "*"));
+    if (is_text)
     {
-      g_mime_multipart_foreach (GMIME_MULTIPART(o),
-                                get_utf8_body_foreach_part,
-                                user_data);
-    }
-    else
-    {
-      GMimePart * part = GMIME_PART (o);
-      GMimeContentType * type = g_mime_object_get_content_type (o);
-      const bool is_text (g_mime_content_type_is_type (type, "text", "*"));
-      if (is_text)
-      {
-        ForeachPartData *data (static_cast<ForeachPartData*>(user_data));
-        data->body += mime_part_to_utf8 (part, data->fallback_charset.c_str());
-      }
+      ForeachPartData *data (static_cast<ForeachPartData*>(user_data));
+      data->body += mime_part_to_utf8 (part, data->fallback_charset.c_str());
     }
   }
 
